327e81e4e5e754e39b8d41d3e7509730ddbbbe4e
hodarasaei83 <hodarasaei83@gmail.com>
2025-12-16 19:40:20 +0330

    Add/union examples

--- app.js
+++ app.js
@@ -8,11 +8,23 @@ function UserData(name, age) {
     console.log("UserAge:", age);
 }
 UserData('Hoda', 21);
+UserData('X');
 //with type for return
 function Counter(a, b) {
+    if (a === void 0) { a = 10; }
     var counted = a + b;
     console.log(counted);
     return counted;
 }
 Counter(12, 5);
 var user1 = { name: "hoda", age: 21 };
+function typeId(id) {
+    if (typeof id === 'string') {
+        console.log('string', id);
+    }
+    else {
+        console.log("number", id);
+    }
+}
+typeId(1);
+var mapDirection = "west";
--- app.ts
+++ app.ts
@@ -4,14 +4,15 @@ console.log(message)
 
 //types in functions
 //without type for return
-function UserData(name:string, age:number){
+function UserData(name:string, age? :number){
     console.log('UserName:',name)
     console.log("UserAge:",age)
 }
 UserData('Hoda',21)
+UserData('X')
 
 //with type for return
-function Counter(a:number,b:number):number{
+function Counter(a:number=10,b:number):number{
   const counted = a + b
   console.log(counted)
   return counted
@@ -21,3 +22,21 @@ Counter(12,5)
 //type alias
 type person = {name:string, age:number}
 const user1:person = {name:"hoda" ,age:21}
+
+//Unions
+type id = string | number
+function typeId(id) {
+  if (typeof id === 'string') {
+    console.log('string',id)
+  }else {console.log("number",id)}
+}
+typeId(1)
+
+//value unions
+type direction = 'north' | 'south' | 'west' | 'east'
+const mapDirection:direction = "west"
+
+//template literal type
+type group = 'Gryffindor' | 'Hufflepuff' | 'Ravenclaw' | 'Slytherin'
+type students = 'Harry' | 'Hermione' | 'Ron'
+type options = `${students} in ${group}`
